d61616b60b6d454f65dc7fe26b1f609a71b4814f
VStefanov89 <vstefanovofm@gmail.com>
2022-08-09 22:31:24 +0300

    Creating our Items

--- crawling/crawling/items.py
+++ crawling/crawling/items.py
@@ -1,12 +1,9 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class CrawlingItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    date = scrapy.Field()
+    name = scrapy.Field()
+    link = scrapy.Field()
+    labels = scrapy.Field()
+    content = scrapy.Field()
